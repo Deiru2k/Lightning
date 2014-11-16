9c55fea1063abb84ef9a5e40550dfc21e523abb0
Илья Панфёров <ilya@Ilas-iMac.local>
2014-11-16 23:00:44 +0300

    Added invite redeem handler to routes

--- routes.py
+++ routes.py
@@ -1,13 +1,18 @@
 from tornado.web import StaticFileHandler
-from handlers import boards, posts, threads
+from handlers import boards, posts, threads, auth
 import os
 
 handlers = [
+    # Media Content
     (r'/v1/images/(.*)', StaticFileHandler, {'path': os.path.join(os.path.dirname(__file__), 'images')}),
+    # Boards
     (r'/v1/boards', boards.BoardsHandler),
     (r'/v1/boards/(\w+)', boards.BoardHandler),
+    # Threads
     (r'/v1/boards/(\w+)/threads', threads.ThreadsHandler),
     (r'/v1/boards/(\w+)/threads/(.*)', threads.ThreadHandler),
-    #(r'/v1/posts', posts.PostsHandler)
-    (r'/v1/posts/(.*)', posts.PostHandler)
+    # Posts
+    (r'/v1/posts/(.*)', posts.PostHandler),
+    # Auth
+    (r'/v1/auth/redeem', auth.RedeemInviteHandler)
 ]
